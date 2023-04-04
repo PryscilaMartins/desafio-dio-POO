7833ae3548f6d7427925c69d39b0b77e25692072
Pryscila Martins <103007680+PryscilaMartins@users.noreply.github.com>
2023-04-04 20:45:38 -0300

    upload

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
 
 
-public class Main {
+publ/**
+     * @param args
+     */
+    /**
+     * @param args
+     */
+    ic class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
@@ -22,12 +30,33 @@ public class Main {
         mentoria.setDescricao("descrição mentoria em java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao(" descrição bootcamp java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPryscila = new Dev();
+        devPryscila.setNome("Pryscila");
+        devPryscila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos" + devCamila.getConteudosInscritos());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devJoao.getConteudosInscritos());
 
 
 
+
+
+         
 
 
 
